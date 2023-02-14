ce9a3d777d38c28e2c42ed6233123bcde735e7d2
Ivan Martinez von Halle <ivanmvh@gmail.com>
2023-02-14 00:42:36 -0500

    Add more categories

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -14,6 +14,20 @@ const Home = () => {
     { categoryCode: 'science', categoryName: 'science', page: 0 },
     { categoryCode: 'education', categoryName: 'education', page: 0 },
     { categoryCode: 'feelings', categoryName: 'feelings', page: 0 },
+    { categoryCode: 'health', categoryName: 'health', page: 0 },
+    { categoryCode: 'people', categoryName: 'people', page: 0 },
+    { categoryCode: 'religion', categoryName: 'religion', page: 0 },
+    { categoryCode: 'places', categoryName: 'places', page: 0 },
+    { categoryCode: 'animals', categoryName: 'animals', page: 0 },
+    { categoryCode: 'industry', categoryName: 'industry', page: 0 },
+    { categoryCode: 'computer', categoryName: 'computer', page: 0 },
+    { categoryCode: 'food', categoryName: 'food', page: 0 },
+    { categoryCode: 'sports', categoryName: 'sports', page: 0 },
+    { categoryCode: 'people', categoryName: 'people', page: 0 },
+    { categoryCode: 'transportation', categoryName: 'transportation', page: 0 },
+    { categoryCode: 'travel', categoryName: 'travel', page: 0 },
+    { categoryCode: 'buildings', categoryName: 'buildings', page: 0 },
+    { categoryCode: 'music', categoryName: 'music', page: 0 },
   ];
 
   const handleClick = (code, page, name) => {
@@ -53,7 +67,9 @@ const Home = () => {
               to={`./category/${category.categoryCode}`}
               key={category.categoryCode}
               onClick={() => handleClick(
-                category.categoryCode, category.page, category.categoryName,
+                category.categoryCode,
+                category.page,
+                category.categoryName,
               )}
               className={`categoryLink ${category.categoryCode}`}
             >
